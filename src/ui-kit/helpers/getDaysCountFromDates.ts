5a499114d071374d7e06c69b535213c7b14a71bc
slitefull <viptitechko1@gmail.com>
2022-09-29 01:04:52 +0300

    update storybook

--- src/ui-kit/helpers/getDaysCountFromDates.ts
+++
@@ -1,4 +0,0 @@
-export const getDaysCountFromDates = (firstDate: Date, secondDate: Date): number => {
-  const difference = firstDate.getTime() - secondDate.getTime();
-  return Math.abs(Math.ceil(difference / (1000 * 3600 * 24)));
-}
--- src/ui-kit/helpers/getFormatDate.ts
+++
@@ -1,4 +0,0 @@
-import { formatISO } from "date-fns";
-
-
-export const getFormatDate = (date: Date): string => formatISO(date).substring(0, 10);
