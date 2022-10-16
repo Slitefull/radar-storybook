983936c4f4f3cd12961767d70b98b85e809fcbbe
slitefull <viptitechko1@gmail.com>
2022-10-16 19:35:25 +0300

    update storybook

--- src/pages/monitor-changes/monitor-changes.tsx
+++ src/pages/monitor-changes/monitor-changes.tsx
@@ -3,7 +3,6 @@ import loadable from "@loadable/component";
 import PageContainer from "@/ui-kit/components/page-container/simple/simple";
 import CardsSectionFallback from "@/pages/monitor-changes/components/cards-section/fallback/fallback";
 import MonitorTableFallback from "@/pages/monitor-changes/components/table/fallback/fallback";
-import Collapsible from "@/ui-kit/components/cards/collapsible/collapsible";
 
 
 const CardsSection = loadable(() => import("./components/cards-section/cards-section"), {
@@ -18,11 +17,6 @@ const MonitorChanges: FC = memo((): JSX.Element => {
   return (
     <PageContainer>
       <CardsSection/>
-      <Collapsible title={"Crawl settings"} subTitle={"How SEO Radar fetches your site's pages."}>
-        Consectetur adipiscing elit pellentesque habitant morbi tristique.
-        Pulvinar pellentesque habitant morbi tristique. Vel quam elementum
-        pulvinar etiam. Pulvinar pellentesque habitant morbi tristique senectus
-      </Collapsible>
       <MonitorTable/>
     </PageContainer>
   );
